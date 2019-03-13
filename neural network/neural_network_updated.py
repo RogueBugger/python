8d07ccc7114f86131674d3be319c6c60480e5801
RogueBugger <42600152+RogueBugger@users.noreply.github.com>
2019-03-13 20:36:32 +0530

    Update neural_network_updated.py

--- neural network/neural_network_updated.py
+++ neural network/neural_network_updated.py
@@ -1,51 +1,45 @@
 import random
 acc=0
-def sign_activation(ws):
-    return 1 if ws>=1 else -1
+def RElu(ws):
+    return 1 if ws>=1 else 0
+
 class perceptron:
     weight=[]
-    learning_rate=1
+    learning_rate=0.1
     acc=0
     def __init__(self):
-        self.weight=[random.uniform(0.0,1.9) for i in range(0,2)]
-        #print(f"weight before change{self.weight}")
-        
+        self.weight=[random.uniform(0.0,1.5) for _ in range(0,3)]
+
     def guess(self,inputs):
         sum=0
-        for w, i in self.weight,inputs:
+        for w, i in zip(self.weight,inputs):
             sum=sum+w*i
-            
-        return sign_activation(sum)
+        return RElu(sum)
 
     def train(self, inputs, target):
         gues=self.guess(inputs)
         wk=[]
         k=0
         error= target - gues
-        
-        self.acc+=error/100
-        for w, i  in self.weight, inputs:
+        self.acc+= 1 if target==gues else 0
+        for w, i  in zip(self.weight, inputs):
             self.weight[k]=w+(error*i*self.learning_rate)
             k+=1
-        #print(self.weight)
-         
-            
-
 
 
 class points:
     def __init__(self):
-        self.x,self.y=random.uniform(0.0,1.4),random.uniform(0.0,1.4)
-        self.lable= 1 if self.x>self.y else -1
+        a,b=random.randint(0,5),random.randint(0,5)
+        self.x= 1 if a>=3 else 0
+        self.y= 1 if b>=3 else 0
+        self.lable= 1 if self.x==self.y and self.x==self.y!=0 else 0
 
-        #print("x=",self.x,"y=",self.y,"lable=",self.lable)
 pr=perceptron()
-p=[points() for i in range(0,100)]
+p=[points() for i in range(0,10000)]
 for o in p:
-    pr.train([o.x,o.y],o.lable)
+    pr.train([o.x,o.y,1],o.lable)
 
 
-v=pr.guess([0,1])
+v=pr.guess([1,1,1])
 print(v)
-print(f"weight after change{pr.weight}")
-print(pr.acc)
+print(pr.acc,'10000')
